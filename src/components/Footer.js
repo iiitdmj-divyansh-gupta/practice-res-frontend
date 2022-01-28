d22a8863ad95ef5c8c2696025f523d868e1d1c24
Divyansh Gupta <dgupta@192.168.0.106>
2022-01-28 23:22:30 +0530

    initializing

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
+function Footer() {
+  return (
+    <div className="Footer">
+     
+      Made with &#128147; By Divyansh 
+      <p>Connect with me <a href="https://www.linkedin.com/in/divyansh-gupta-4b0a83192/"> {<LinkedInIcon />}</a> </p>
+    </div>
+  );
+}
+
+export default Footer;
